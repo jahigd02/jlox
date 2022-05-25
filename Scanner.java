352a9d5366228be0f05dab42bd7ef69efc40d42c
Jacob Higdon <jahigd02@louisville.edu>
2022-05-25 00:47:09 -0400

    implemented Scanner class and some associated methods

--- Scanner.java
+++
@@ -1,8 +0,0 @@
-public class Scanner {
-    private final String source;
-    private final List<Token> tokens = new ArrayList<>();
-
-    Scanner(String source) {
-        this.source = source;
-    }
-}
