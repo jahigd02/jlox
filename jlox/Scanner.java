a2e138682c000944d8511c5c8d3ecd1bc047b9d7
Jacob Higdon <jahigd02@louisville.edu>
2022-05-29 16:05:33 -0400

    Scanner class update

--- jlox/Scanner.java
+++ jlox/Scanner.java
@@ -61,10 +61,78 @@ public class Scanner {
             case '*':
                 addToken(STAR);
                 break;
+            case '!':
+                addToken(match('=') ? BANG_EQUAL : BANG);
+                break;
+            case '=':
+                addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+                break;
+            case '>':
+                addToken(match('=') ? GREATER_EQUAL : GREATER);
+                break;
+            case '<':
+                addToken(match('=') ? LESS_EQUAL : LESS);
+                break;
+            case '/':
+                if (match('/')) {
+                    while (peek() != '\n' && !isAtEnd())
+                        advance();
+                } else {
+                    addToken(SLASH);
+                }
+                break;
+
+            case ' ':
+            case '\r':
+            case '\t':
+                break;
+            case '\n':
+                line++;
+                break;
+
+            case '"':
+                string();
+                break;
             default:
                 Lox.error(line, "Unexpected character!");
                 break;
         }
+    }
+
+    private void string() {
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n')
+                line++;
+            advance();
+        }
+
+        if (isAtEnd()) {
+            Lox.error(line, "Unterminated string; missing quotation mark.");
+        }
+
+        // Closing "
+        advance();
+
+        // Trim beginning and end ".
+        String value = source.substring(start + 1, current - 1);
+        addToken(STRING, value);
+    }
+
+    private char peek() {
+        if (isAtEnd())
+            return '\0';
+        return source.charAt(current);
+    }
+
+    private boolean match(char exp) {
+        if (isAtEnd())
+            return false;
+        if (source.charAt(current) != exp)
+            return false;
+
+        current++;
+        return true;
+
     }
 
     private boolean isAtEnd() {
